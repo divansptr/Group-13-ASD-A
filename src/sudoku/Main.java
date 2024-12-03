490af575f4257065f34b0a3373a65ef43775e458
Diva <dvnsputri@gmail.com>
2024-12-03 09:45:30 +0700

    init commit

---
+++ src/sudoku/Main.java
@@ -0,0 +1,10 @@
+package sudoku;
+
+public class Main {
+    /** The entry main() entry method */
+    public static void main(String[] args) {
+        // [TODO 1] Check "Swing program template" on how to run
+        //  the constructor of "Sudoku"
+            new Sudoku(); // Create and display the main Sudoku GUI
+    }
+}
