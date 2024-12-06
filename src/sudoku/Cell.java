cb4d98b5e02fcb932ec0f248451d3613ba1b80a7
Adelia <adeliaparamitaptri@gmail.com>
2024-12-06 19:29:40 +0700

    Update font and highlight

--- src/sudoku/Cell.java
+++ src/sudoku/Cell.java
@@ -19,7 +19,7 @@ public class Cell extends JTextField {
     public static final Color BG_TO_GUESS  = Color.YELLOW;
     public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
     public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
-    public static final Color HIGHLIGHT_COLOR  = new Color(135, 206, 250); //tambahan beautify
+    public static final Color HIGHLIGHT_COLOR  = new Color(135, 206, 250); //tambahan untuk beauty
 
     // Beautify font
     public static final Font FONT_NUMBERS = new Font("Poppins", Font.BOLD, 23);
